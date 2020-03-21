f4c616493753a3c39f57d2eb38ad6d87efc2d30f
Valerie <valerie.vaske@gmail.com>
2020-03-21 13:35:41 +0100

    Initial Setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# WirVsVirus
-WirVsVirus Hackathon project
---
+++ contracts/DigiMark.sol
@@ -0,0 +1,46 @@
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+	  function approve(address spender, uint tokens)public returns (bool success);
+    function transferFrom (address from, address to, uint tokens) public returns (bool success);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b; }
+
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b); }
+
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+contract DigiMark is ERC20Interface, SafeMath {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    uint256 public _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor() public {
+        name = "DigiMark";
+        symbol = "DM";
+        decimals = 9;
+    }
+}
