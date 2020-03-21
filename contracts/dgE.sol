e4ed69fdce16e0ec8ec189d2064f18c2b23beec2
Valerie <valerie.vaske@gmail.com>
2020-03-21 17:47:42 +0100

    Kompiliert *_*

--- contracts/dgE.sol
+++ contracts/dgE.sol
@@ -6,15 +6,16 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract dgE is ERC20 {
 
-    using SafeMath for uint; 
+    using SafeMath for uint256;
+
     string public name;
     string public symbol;
     uint8 public decimals;
 
-    uint256 public _totalSupply;
+    uint256 private _totalSupply;
 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowed;
 
     constructor() public {
         name = "Dezentraler gemeinschaftsEuro";
@@ -22,42 +23,59 @@ contract dgE is ERC20 {
         decimals = 18;
     }
 
-    // determine total number of tokens floating around
-    function totalSupply() public view returns (uint) {
-      return _totalSupply - balances[address(0)];
+    /**
+     * @dev Total number of tokens in existence
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
     }
 
-    // returns number of token from specific address
-    function balanceOf(address _address) public view returns (uint balance) {
-      return balances[_address];
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param owner The address to query the balance of.
+     * @return An uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
     }
 
-    // checks if transaction is allowed
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
-      return allowed[tokenOwner][spender];
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
     }
 
-    // approve checked transaction
-    function approve(address spender, uint tokens) public returns (bool success) {
-      allowed[msg.sender][spender] = tokens;
-      emit Approval(msg.sender, spender, tokens);
-      return true;
+    /**
+     * @dev Transfer token for a specified address
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
     }
 
-    // basic transfer function
-    // needs to be modified to only send to whitelisted business accounts
-    function transfer(address to, uint tokens) public returns (bool success) {
-      balances[msg.sender] = add(balances[to], tokens);
-      emit Transfer(msg.sender, to, tokens);
-      return true;
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
     }
 
-    // automates transfer function to specific account
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-      balances[from] = sub(balances[from][msg.sender], tokens);
-      balances[to] = add(balances[to], tokens);
-      emit Transfer(from, to, tokens);
-      return true;
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not required as per the specification,
+     * and other compliant implementations may not emit the event.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        _transfer(from, to, value);
+        _approve(from, msg.sender, _allowed[from][msg.sender].sub(value));
+        return true;
     }
 
 }
