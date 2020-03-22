702dc9cc09c7bf6bca7c99d2b0ab1c494dfaa5ff
Frederik Lührs <luehrs.fred@gmail.com>
2020-03-22 15:29:24 +0100

    whitelist with not authentication self-add

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -31,7 +31,7 @@ contract Whitelist is Ownable {
    * @param addr address
    * @return true if the address was added to the whitelist, false if the address was already in the whitelist
    */
-  function addAddressToWhitelist(address addr) public returns(bool success) {
+  function addAddressToWhitelist(address addr) public onlyOwner returns(bool success) {
     if (!whitelist[addr]) {
       whitelist[addr] = true;
       emit WhitelistedAddressAdded(addr);
@@ -50,6 +50,15 @@ contract Whitelist is Ownable {
       if (addAddressToWhitelist(addrs[i])) {
         success = true;
       }
+    }
+  }
+
+  function addToWhitelist() public returns(bool success){
+    address addr = _msgSender();
+    if (!whitelist[addr]) {
+      whitelist[addr] = true;
+      emit WhitelistedAddressAdded(addr);
+      success = true;
     }
   }
 
