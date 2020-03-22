d0ab6cae869e4f7cae356e84e305b44f5057b377
Frederik Lührs <luehrs.fred@gmail.com>
2020-03-22 13:46:22 +0100

    add build

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -31,7 +31,7 @@ contract Whitelist is Ownable {
    * @param addr address
    * @return true if the address was added to the whitelist, false if the address was already in the whitelist
    */
-  function addAddressToWhitelist(address addr) public onlyOwner returns(bool success) {
+  function addAddressToWhitelist(address addr) public returns(bool success) {
     if (!whitelist[addr]) {
       whitelist[addr] = true;
       emit WhitelistedAddressAdded(addr);
